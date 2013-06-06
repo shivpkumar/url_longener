abd975950c846fdaccd276c3009460bb72f5556b
Shiv Kumar <shivpkumar@gmail.com>
2013-06-05 19:25:33 -0700

    create profile/login pages and routes

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,6 +3,17 @@ get '/' do
   erb :index
 end
 
+get '/user/:id' do
+  user_id = params[:id]
+  @user = User.find(user_id)
+  @urls = Url.where("user_id = ?", user_id).reverse
+  erb :profile
+end
+
+get '/login' do
+  erb :login
+end
+
 get '/:long_url_hash' do
   @url = Url.find_by_long_url_hash(params[:long_url_hash])
   @url.click_count += 1
